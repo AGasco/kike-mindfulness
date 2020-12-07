7e032e662bbec02b3a32b8e094aaf276981d2a13
AGasco <antoniofgasco@gmail.com>
2020-12-07 13:02:42 +0000

    landing-button

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,12 +14,8 @@ function Landing() {
           <p>kike@gmail.com - 642042042</p>
         </div>
         <h1>KIKE MINDFULNESS</h1>
-        {/* <h4>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-          expedita, magnam quis possimus rem itaque fuga obcaecati
-          necessitatibus minus asperiores maxime, id voluptatum minima aliquid
-          beatae repellendus debitis ut. Autem.
-        </h4> */}
+        <h6>Aprende a meditar y recupera tu sensación de paz y bienestar</h6>
+        <button>RESERVA AHORA</button>
       </div>
     </div>
   );
