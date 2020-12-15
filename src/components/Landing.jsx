f1d41bc0758b553a2ab839c6749de971f00e5d4a
AGasco <antoniofgasco@gmail.com>
2020-12-15 10:37:25 +0000

    data.json

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,7 +6,7 @@ function Landing() {
   return (
     <div className="landing">
       <div className="landing__mainSide">
-        <p>kike@gmail.com - 642042042</p>
+        <p>kikemindfulness@gmail.com - 620648806</p>
       </div>
       <h1>KIKE MINDFULNESS</h1>
       <h6>Aprende a meditar y recupera tu sensación de paz y bienestar</h6>
